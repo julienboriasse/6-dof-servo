938b370a7df7234859976b2bf59ca748d296c4a6
Julien Boriasse <julien@boriasse.com>
2022-12-23 11:46:02 -0500

    Cleanup debug message

--- lib/Servo/src/servo.cpp
+++ lib/Servo/src/servo.cpp
@@ -30,8 +30,6 @@ float Servo::pulse_width(float angle)
     // limit pulse width range
     pulseWidth = pulseWidth > maxPulseWidth ? maxPulseWidth : pulseWidth;
     pulseWidth = pulseWidth < minPulseWidth ? minPulseWidth : pulseWidth;
-    
-    printf("%.5f %.5f\n", angle, pulseWidth);
 
     return pulseWidth;
 }
